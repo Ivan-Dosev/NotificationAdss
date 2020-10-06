e04ef54c92a76d1f4f49c1b48bf08ab3f8e0c127
Ivan Dimitrov <biobeehive@abv.bg>
2020-10-07 00:16:35 +0300

    Initial Commit

---
+++ NotificationAdss/NotificationAdssApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationAdssApp.swift
+//  NotificationAdss
+//
+//  Created by Ivan Dimitrov on 7.10.20.
+//
+
+import SwiftUI
+
+@main
+struct NotificationAdssApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
